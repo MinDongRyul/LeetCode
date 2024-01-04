6714d330f24b154c707eee0fa55a2ea390922904
Dongryul Min <101853960+MinDongRyul@users.noreply.github.com>
2024-01-04 19:42:41 +0900

    Time: 23 ms (23.85%), Space: 13.5 MB (55.27%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        len_strs = [len(str_) for str_ in strs]
+        
+        min_len = min(len_strs)
+
+        strs = [str_[:min_len] for str_ in strs]
+        for idx in range(min_len, 0, -1):
+            temp = [str_[:idx] for str_ in strs]
+            if len(set(temp)) == 1:
+                return temp[0]
+        return ""
