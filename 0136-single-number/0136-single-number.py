865f83929da69b0129106b92d6086da67de1ffd5
Dongryul Min <101853960+MinDongRyul@users.noreply.github.com>
2024-01-05 08:59:03 +0900

    Time: 101 ms (52.05%), Space: 16.3 MB (13.07%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -4,19 +4,11 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        #dict = {}
-        #for i in nums:
-        #    if i not in dict:
-        #        dict[i] = 1
-        #    else:
-        #        dict.pop(i)
-        #return int(*dict)
-        num_dict = {}
-        for num in nums:
-            if str(num) not in num_dict.keys():
-                num_dict[str(num)] = 1
+        dict = {}
+        for i in nums:
+            if i not in dict:
+                dict[i] = 1
             else:
-                num_dict[str(num)] += 1
-        min_nums = min(num_dict, key=lambda k: num_dict[k])
-        return int(min_nums)
+                dict.pop(i)
+        return int(*dict)
         
