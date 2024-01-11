8b5f85ac7e9a024161c7777e7ec03708ce093497
Dongryul Min <101853960+MinDongRyul@users.noreply.github.com>
2024-01-11 20:13:08 +0900

    Time: 13 ms (60.34%), Space: 13.2 MB (61.51%) - LeetHub

---
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def convertToTitle(self, columnNumber):
+        """
+        :type columnNumber: int
+        :rtype: str
+        """
+        x = columnNumber
+        temp = x
+
+        cnt = 0
+        while x > 26:
+            x //= 26
+            cnt += 1
+
+        rst = []
+        for idx in range(cnt, -1, -1):
+            rst.append(temp // (26 ** idx))
+            temp %= (26 ** idx)
+            
+        for idx in range(len(rst)-1):
+            if rst[idx+1] == 0:
+                rst[idx], rst[idx+1] = rst[idx]-1, 26
+
+        for idx in range(len(rst)-1):
+            if rst[idx] == 1 and rst[idx+1] == 0:
+                rst[idx+1] = 26
+                rst[idx] = 'x'
+            elif rst[idx+1] == 0:
+                rst[idx+1] = 26
+                rst[idx] -= 1
+                
+        return ''.join([chr(num+64) for num in rst if num != 'x'])
