b7a7a82b3952c17338b124dbd3ca5b5c9cca01d2
Dongryul Min <101853960+MinDongRyul@users.noreply.github.com>
2024-01-19 14:14:11 +0900

    Time: 19 ms (32.99%), Space: 13.4 MB (49.24%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        lst = [str_ for str_ in s]
+ 
+        stack = [s[0]]
+
+        for s in lst[1:]:
+            if len(stack) != 0 and stack[-1]+s in ['()', '{}', '[]']:
+                stack.pop(-1)
+            else:
+                stack.append(s)
+            
+        if len(stack) == 0:
+            return True
+        else:
+            return False
