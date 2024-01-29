ab52e02733c3505ce914276200ea59b5e0825eab
Dongryul Min <101853960+MinDongRyul@users.noreply.github.com>
2024-01-29 22:54:42 +0900

    Time: 12 ms (84.68%), Space: 11.6 MB (87.85%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.py
+++ 0263-ugly-number/0263-ugly-number.py
@@ -10,15 +10,12 @@ class Solution(object):
         elif n <= 0:
             return False
         else:
-            factors = []
             i = 2
             while i <= n:
                 if n % i == 0:
-                    factors.append(i)
                     n = n / i
                 else:
                     i = i + 1
-
                 if i >= 6:
                     return False
         return True
