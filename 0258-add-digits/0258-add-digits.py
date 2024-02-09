bae597f8ffb23064bcfccd66b05df3e596cf8eb9
Dongryul Min <101853960+MinDongRyul@users.noreply.github.com>
2024-02-09 13:24:31 +0900

    Time: 24 ms (16.85%), Space: 11.4 MB (100%) - LeetHub

--- 0258-add-digits/0258-add-digits.py
+++ 0258-add-digits/0258-add-digits.py
@@ -1,6 +1,13 @@
-def totalReturn(num):
+# def totalReturn(num):
+#     temp = [int(val) for val in str(num)]
+#     total = sum(temp)
+#     return total
+
+def totalReturn2(num):
     temp = [int(val) for val in str(num)]
     total = sum(temp)
+    if total >= 10:
+        return totalReturn2(total)
     return total
 
 class Solution(object):
@@ -9,8 +16,8 @@ class Solution(object):
         :type num: int
         :rtype: int
         """
-        total = totalReturn(num)
-        while total >= 10:
-            total = totalReturn(num)
-            num = total
+        total = totalReturn2(num)
+        # while total >= 10:
+        #     total = totalReturn(num)
+        #     num = total
         return total
