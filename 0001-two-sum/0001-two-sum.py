ad9eb2a602e5ed48c700f1a574205b41f87ac0f5
Dongryul Min <101853960+MinDongRyul@users.noreply.github.com>
2024-02-10 14:00:45 +0900

    Time: 288 ms (53.2%), Space: 12.5 MB (83.1%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -5,13 +5,15 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        rst = []
-        for idx in range(len(nums)):
-            a = nums[idx]
-            for idx_2 in range(idx+1, len(nums)):
-                b = nums[idx_2]
-                if a + b == target:
-                    rst.append(idx)
-                    rst.append(idx_2)
-        return rst
-        
+        # rst = []
+        # for idx in range(len(nums)):
+        #     a = nums[idx]
+        #     for idx_2 in range(idx+1, len(nums)):
+        #         b = nums[idx_2]
+        #         if a + b == target:
+        #             rst.append(idx)
+        #             rst.append(idx_2)
+        #             return rst
+        for idx, val in enumerate(nums):
+            if target - val in nums[idx+1:]:
+                return [idx, nums.index(target-val, idx+1, len(nums))]
