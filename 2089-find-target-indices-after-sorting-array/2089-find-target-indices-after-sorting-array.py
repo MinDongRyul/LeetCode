aa47b9ee13aea972a09f7ee50150e06c8aa8e81a
Dongryul Min <101853960+MinDongRyul@users.noreply.github.com>
2024-03-05 20:47:00 +0900

    Time: 29 ms (49.06%), Space: 11.8 MB (31.77%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.py
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.py
@@ -5,9 +5,20 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        ret = []
-        nums.sort()
-        for idx, val in enumerate(nums):
-            if val == target:
-                ret.append(idx)
-        return ret
+        # ret = []
+        # nums.sort()
+        # for idx, val in enumerate(nums):
+        #     if val == target:
+        #         ret.append(idx)
+        # return ret
+
+        # target 보다 큰 숫자는 무시하며 작은 숫자는 lt_count 갯수에 추가
+        # target와 같은 숫자는 eq_count 갯수에 추가
+        lt_count = eq_count = 0
+        for n in nums:
+            if n < target:
+                lt_count += 1
+            elif n == target:
+                eq_count += 1
+            
+        return list(range(lt_count, lt_count+eq_count))
