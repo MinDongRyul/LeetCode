37a1e99154cb7a193bee467babeaf063da7c66b5
Dongryul Min <101853960+MinDongRyul@users.noreply.github.com>
2024-03-05 21:21:45 +0900

    Time: 19 ms (37.95%), Space: 11.5 MB (80.98%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        for val in range(1, len(nums)):
+            key = nums[val]
+            i = val - 1
+            while i >= 0 and nums[i] > key:
+                nums[i + 1] = nums[i]
+                i = i - 1
+            nums[i+1] = key
