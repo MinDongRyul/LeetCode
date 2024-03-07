7cc411f30126d33d467b9ade1292c01857e4275c
Dongryul Min <101853960+MinDongRyul@users.noreply.github.com>
2024-03-07 20:07:45 +0900

    Time: 61 ms (47.96%), Space: 13.4 MB (44.66%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -4,17 +4,30 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        ret = 0
-        remove = []
-        for idx, val in enumerate(nums):
-            if idx == nums.index(val):
-                ret += 1
-            else:
-                remove.append(idx)
+        # temp = set()
+        # for val in nums:
+        #     temp.add(nums.index(val))
+        # nums[:] = [nums[val] for val in temp]
+        # return len(temp)
+
+        # ret = 0
+        # remove = []
+        # for idx, val in enumerate(nums):
+        #     if idx == nums.index(val):
+        #         ret += 1
+        #     else:
+        #         remove.append(idx)
         
-        remove.sort(reverse=True)
+        # remove.sort(reverse=True)
                 
-        for remove_num in remove:
-            nums.pop(remove_num)
+        # for remove_num in remove:
+        #     nums.pop(remove_num)
                 
-        return ret
+        # return ret
+
+        j = 1
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i - 1]:
+                nums[j] = nums[i]
+                j += 1
+        return j
