0f15fc4c356330c2d2831e07057579a249a1c06f
Dongryul Min <101853960+MinDongRyul@users.noreply.github.com>
2024-03-09 22:31:53 +0900

    Time: 207 ms (19.22%), Space: 12.9 MB (57.96%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.py
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.py
@@ -5,9 +5,9 @@ class Solution(object):
         :rtype: int
         """
         nums = sorted(nums)        
-        len_nums = len(nums)
-        
-        max_val_1 = nums[0] * nums[1] * nums[len_nums-1]
-        max_val_2 = nums[len_nums-1] * nums[len_nums-2] * nums[len_nums-3]
+        # len_nums = len(nums)
+        max_val_1 = nums[0] * nums[1] * nums[-1]
+        # max_val_2 = nums[len_nums-1] * nums[len_nums-2] * nums[len_nums-3]
+        max_val_2 = nums[-1] * nums[-2] * nums[-3]
         
         return max(max_val_1, max_val_2)
