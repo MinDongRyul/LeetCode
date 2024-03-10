1fd25c129fbb11f561de495e01c7845e8c623fb2
Dongryul Min <101853960+MinDongRyul@users.noreply.github.com>
2024-03-10 14:04:31 +0900

    Time: 15 ms (82.98%), Space: 11.7 MB (57.41%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -11,3 +11,22 @@ class Solution(object):
             nums1[i+m] = nums2[i]
             
         nums1.sort()
+
+        # 1. n의 갯수 만큼 앞을 먼저 비교
+        # 2. n이 0이 아니면 그만큼 뒤에 추가
+        
+        # if n == 0 :return
+        # len1 = len(nums1)
+        # end_idx = len1-1
+        # while n > 0 and m > 0 :
+        #     if nums2[n-1] >= nums1[m-1]:
+        #         nums1[end_idx] = nums2[n-1]
+        #         n-=1
+        #     else:
+        #         nums1[end_idx] = nums1[m-1]
+        #         m-=1
+        #     end_idx-=1
+        # while n > 0:
+        #     nums1[end_idx] = nums2[n-1]
+        #     n-=1
+        #     end_idx-=1
