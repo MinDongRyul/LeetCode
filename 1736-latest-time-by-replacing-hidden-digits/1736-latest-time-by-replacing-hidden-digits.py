7a41708e5140daa8bc059b9e84f97d145814f9e0
Dongryul Min <101853960+MinDongRyul@users.noreply.github.com>
2024-03-12 21:43:06 +0900

    Time: 53 ms (12.5%), Space: 11.7 MB (46.88%) - LeetHub

--- 1736-latest-time-by-replacing-hidden-digits/1736-latest-time-by-replacing-hidden-digits.py
+++ 1736-latest-time-by-replacing-hidden-digits/1736-latest-time-by-replacing-hidden-digits.py
@@ -15,3 +15,20 @@ class Solution(object):
                 latest_time = h + ':' + m
                 if p.match(latest_time):
                     return latest_time
+
+        # faster answer
+        # time = list(time)  # Convert to list for easier manipulation
+
+        # # Handling the first two characters for hours
+        # if time[0] == '?':
+        #     time[0] = '2' if time[1] in '?0123' else '1'
+        # if time[1] == '?':
+        #     time[1] = '3' if time[0] == '2' else '9'
+
+        # # Handling the last two characters for minutes
+        # if time[3] == '?':
+        #     time[3] = '5'
+        # if time[4] == '?':
+        #     time[4] = '9'
+
+        # return ''.join(time) 
