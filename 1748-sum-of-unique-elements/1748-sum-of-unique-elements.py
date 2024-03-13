8500847ad090fe521de20f1b6e5780464d1da445
Dongryul Min <101853960+MinDongRyul@users.noreply.github.com>
2024-03-13 20:24:36 +0900

    Time: 24 ms (10.07%), Space: 11.6 MB (78.72%) - LeetHub

--- 1748-sum-of-unique-elements/1748-sum-of-unique-elements.py
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.py
@@ -6,9 +6,11 @@ class Solution(object):
         """
         nums_dict = {}
         for num in nums:
-            if num not in nums_dict.keys():
+            if num not in nums_dict:
                 nums_dict[num] = 1
             else:
                 nums_dict[num] += 1
-        r = sum([key for key in nums_dict.keys() if nums_dict[key] == 1])
+
+        r = sum([item[0] for item in nums_dict.items() if item[1] == 1])        
+        # r = sum([key for key in nums_dict.keys() if nums_dict[key] == 1])
         return r
