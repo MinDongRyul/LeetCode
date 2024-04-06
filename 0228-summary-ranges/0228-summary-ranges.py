1c640a96bd5682a30cc3210fec12c259b2e83705
Dongryul Min <101853960+MinDongRyul@users.noreply.github.com>
2024-04-06 13:40:32 +0900

    Time: 19 ms (15.66%), Space: 11.7 MB (26.33%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.py
+++ 0228-summary-ranges/0228-summary-ranges.py
@@ -4,25 +4,35 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[str]
         """
-        stack = []
-        temp = []
-        r = []
+        # stack = []
+        # temp = []
+        # r = []
         
-        if not nums:
-            return []
+        # if not nums:
+        #     return []
         
-        for num in nums:
-            if not stack or abs(stack[-1] - num) == 1:
-                stack.append(num)
+        # for num in nums:
+        #     if not stack or abs(stack[-1] - num) == 1:
+        #         stack.append(num)
+        #     else:
+        #         temp.append(stack)
+        #         stack = [num]
+        # temp.append(stack)
+        
+        # r = [str(val[0]) if val[0] == val[-1] else str(val[0]) + "->" + str(val[-1]) for val in temp]
+        
+        # return r
+
+        # 좋은 정답 표본
+        ranges = [] # [start, end] or [x, y]
+        for n in nums:
+            # ex
+            # n : 0 -> ranges.append([0, 0])
+            # n : 1 -> range[-1] : [0, 0], range[-1][1] : 0
+            #       -> range[-1][1] == n - 1 -> range[-1][1] == n 
+            if ranges and ranges[-1][1] == n-1:
+                ranges[-1][1] = n
             else:
-                temp.append(stack)
-                stack = [num]
-        temp.append(stack)
-        
-        for val in temp:
-            if val[0] == val[-1]:
-                r.append(str(val[0]))
-            else:
-                r.append(str(val[0]) + "->" + str(val[-1]))
-        
-        return r
+                ranges.append([n, n])
+        return [str(x) + '->' + str(y) if x != y else str(x) for x, y in ranges]
+
