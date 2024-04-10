2c9aa1012777b816d4b94777e485435f5c74955a
Dongryul Min <101853960+MinDongRyul@users.noreply.github.com>
2024-04-10 12:23:34 +0900

    Time: 1622 ms (98.11%), Space: 11.8 MB (66.98%) - LeetHub

--- 2221-find-triangular-sum-of-an-array/2221-find-triangular-sum-of-an-array.py
+++ 2221-find-triangular-sum-of-an-array/2221-find-triangular-sum-of-an-array.py
@@ -6,8 +6,5 @@ class Solution(object):
         """
         len_cycle = len(nums)
         for _ in range(len_cycle-1):
-            temp = []
-            for i in range(1, len(nums)):
-                temp.append((nums[i-1] + nums[i]) % 10)
-            nums = temp 
+            nums = [(nums[i-1] + nums[i]) % 10 for i in range(1, len(nums))]
         return nums[0]
