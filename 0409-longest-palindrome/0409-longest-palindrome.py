29598c3898f325c83504793ef55b88014a23c0db
Dongryul Min <101853960+MinDongRyul@users.noreply.github.com>
2024-04-19 19:09:47 +0900

    Time: 14 ms (78.09%), Space: 11.7 MB (70.53%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.py
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -2,4 +2,11 @@ import collections
 
 class Solution:
     def longestPalindrome(self, s):
-        return min(sum(count - (count % 2) for count in collections.Counter(s).values()) + 1, len(s))
+        # return min(sum(count - (count % 2) for count in collections.Counter(s).values()) + 1, len(s))temp = {}
+        temp = {}
+        for val in s:
+            if val not in temp:
+                temp[val] = 1
+            else:
+                temp[val] += 1
+        return min(sum((count[1] - (count[1] % 2) for count in temp.items())) + 1, len(s))
