9f1f4436f6e4302ac3f6c9e6596d9bc7579b5abe
Dongryul Min <101853960+MinDongRyul@users.noreply.github.com>
2024-04-24 18:12:28 +0900

    Time: 44 ms (35.78%), Space: 14.1 MB (68.69%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -1,4 +1,5 @@
 from collections import Counter
+import heapq
 
 class Solution(object):
     def frequencySort(self, s):
@@ -6,8 +7,27 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        cs = sorted(Counter(s).items(), key=lambda x : x[1], reverse=True)
-        r = ''
-        for val in cs:
-            r += val[0] * val[1]
-        return r
+        # cs = sorted(Counter(s).items(), key=lambda x : x[1], reverse=True)
+        # solution 1
+        # r = ''
+        # for val in cs:
+        #     r += val[0] * val[1]
+        
+        # solution 2
+        # r = ''.join([val[0] * val[1] for val in cs])
+        # return r
+
+        # solutuon 3
+        # Use Counter to get the frequency of each character
+        char_freq = Counter(s)
+        # Use max heap to sort characters based on frequency
+        max_heap = [(-freq, char) for char, freq in char_freq.items()]
+        heapq.heapify(max_heap)
+        # Build the result string
+        result = ""
+        while max_heap:
+            freq, char = heapq.heappop(max_heap)
+            result += char * (-freq)
+        return result
+        
+        
