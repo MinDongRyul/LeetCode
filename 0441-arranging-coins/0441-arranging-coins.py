792bac7d43131f7c04b767010e057b9970981437
Dongryul Min <101853960+MinDongRyul@users.noreply.github.com>
2024-04-30 07:42:42 -0700

    Time: 22 ms (66.47%), Space: 11.6 MB (72.87%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.py
+++ 0441-arranging-coins/0441-arranging-coins.py
@@ -1,14 +1,19 @@
+from math import sqrt
+
 class Solution(object):
     def arrangeCoins(self, n):
         """
         :type n: int
         :rtype: int
         """
-        i = 0
-        while True:
-            i += 1
-            n = n - i
-            if n < 0:
-                return i-1
-            elif n == 0:
-                return i
+        # 메모리 비효율적
+        # i = 0
+        # while True:
+        #     i += 1
+        #     n = n - i
+        #     if n < 0:
+        #         return i-1
+        #     elif n == 0:
+        #         return i
+
+        return int((sqrt(8 * n + 1) - 1) / 2)
