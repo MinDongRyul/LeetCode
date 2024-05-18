2de29f301f649506968988726a64c77238c0c29b
Dongryul Min <101853960+MinDongRyul@users.noreply.github.com>
2024-05-18 23:21:39 +0900

    Time: 49 ms (84.55%), Space: 14.1 MB (24.39%) - LeetHub

--- 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.py
+++ 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.py
@@ -1,24 +1,29 @@
+from collections import Counter
+
 class Solution(object):
     def findPairs(self, nums, k):
         """
         :type nums: List[int]
         :type k: int
         :rtype: int
-        
-        ex)
-        nums = [3,1,4,1,5]
-        k = 2
-        r = 2
-        
-        nums = [1,2,3,4,5]
-        k = 1
-        output = 4
         """
-        r = set()
-        for i in range(len(nums)):
-            val1, val2 = nums[i]+k, nums[i]-k
-            if val1 in nums[i+1:]:
-                r.add((nums[i], val1))
-            if val2 in nums[i+1:]:
-                r.add((val2, nums[i]))
-        return len(r)
+        # r = set()
+        # for i in range(len(nums)):
+        #     val1, val2 = nums[i]+k, nums[i]-k
+        #     if val1 in nums[i+1:]:
+        #         r.add((nums[i], val1))
+        #     if val2 in nums[i+1:]:
+        #         r.add((val2, nums[i]))
+        # return len(r)
+        freq_count = Counter(nums)
+        k_pair_count = 0
+
+        for key, value in freq_count.items():
+            if k == 0:
+                if value > 1:
+                    k_pair_count += 1
+            else:
+                if (key + k) in freq_count:
+                    k_pair_count += 1
+        
+        return k_pair_count
