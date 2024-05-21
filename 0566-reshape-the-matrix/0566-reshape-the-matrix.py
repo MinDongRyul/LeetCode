feff9086bdcbdc5016b384b3aba83b5287d6df21
Dongryul Min <101853960+MinDongRyul@users.noreply.github.com>
2024-05-21 21:12:18 +0900

    Time: 52 ms (92.86%), Space: 12.3 MB (51.19%) - LeetHub

---
+++ 0566-reshape-the-matrix/0566-reshape-the-matrix.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def matrixReshape(self, mat, r, c):
+        """
+        :type mat: List[List[int]]
+        :type r: int
+        :type c: int
+        :rtype: List[List[int]]
+        """
+        if len(mat) * len(mat[0]) == r * c:
+            output = []
+            for mat_ in mat:
+                output += mat_
+            return [output[idx*c:(idx+1)*c] for idx in range(0, r)]
+        else:
+            return mat
