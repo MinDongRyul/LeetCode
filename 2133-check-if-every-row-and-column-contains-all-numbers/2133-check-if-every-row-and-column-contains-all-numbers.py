1a4e820a1c6d1832d74d6eda94885762b312ac7a
Dongryul Min <101853960+MinDongRyul@users.noreply.github.com>
2024-05-25 00:25:12 +0900

    Time: 577 ms (82.54%), Space: 12.2 MB (33.33%) - LeetHub

--- 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.py
+++ 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.py
@@ -6,15 +6,18 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: bool
         """
-        row, col = defaultdict(int), defaultdict(int)
+        # row, col = defaultdict(int), defaultdict(int)
         
-        for x in range(len(matrix)):
-            for y in range(len(matrix[0])):
-                row[matrix[x][y]] += 1 # row -> column
-                col[matrix[y][x]] += 1 # column -> row
+        # for x in range(len(matrix)):
+        #     for y in range(len(matrix[0])):
+        #         row[matrix[x][y]] += 1 # row -> column
+        #         col[matrix[y][x]] += 1 # column -> row
 
-            for row_, col_ in zip(row, col):
-                if row[row_] > x+1 or col[col_] > x+1:
-                    return False
+        #     for row_, col_ in zip(row, col):
+        #         if row[row_] > x+1 or col[col_] > x+1:
+        #             return False
                 
-        return True
+        # return True
+
+        set_ = set(range(1,len(matrix)+1))
+        return all(set_ == set(x) for x in matrix+list(zip(*matrix)))
