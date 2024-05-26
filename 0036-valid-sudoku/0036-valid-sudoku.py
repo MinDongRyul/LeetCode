9d853ff06fa83a8372b217ca0edaf5447eed0d23
Dongryul Min <101853960+MinDongRyul@users.noreply.github.com>
2024-05-26 21:59:37 +0900

    Time: 65 ms (47.4%), Space: 11.7 MB (38.6%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -0,0 +1,31 @@
+def check_row_col(matrix):
+    for row in matrix:
+        for idx, row_ in enumerate(row):
+            if row_ in row[idx+1:] and row_.isdecimal():
+                return False
+    return True
+
+class Solution(object):
+    def isValidSudoku(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: bool
+        """
+        # row, col check
+        # 9x9 board check
+        # False -> return False
+        if not check_row_col(board) or not check_row_col(list(zip(*board))): return False
+
+        # 3x3 board check
+        temp_1, temp_2, temp_3 = [], [], []
+        for idx, row in enumerate(board):
+            temp_1 += row[:3]
+            temp_2 += row[3:6]
+            temp_3 += row[6:]
+            if idx % 3 == 2:
+                if check_row_col([temp_1]) and check_row_col([temp_2]) and check_row_col([temp_3]):
+                    temp_1, temp_2, temp_3 = [], [], []
+                else:
+                    return False
+        
+        return True
