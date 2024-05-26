07b2ab6b4561dd3ca10b1492ed5d4d1a48c8557f
Dongryul Min <101853960+MinDongRyul@users.noreply.github.com>
2024-05-26 22:10:41 +0900

    Time: 59 ms (79.85%), Space: 11.7 MB (72.49%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -14,18 +14,29 @@ class Solution(object):
         # row, col check
         # 9x9 board check
         # False -> return False
-        if not check_row_col(board) or not check_row_col(list(zip(*board))): return False
+        # if not check_row_col(board) or not check_row_col(list(zip(*board))): return False
 
-        # 3x3 board check
-        temp_1, temp_2, temp_3 = [], [], []
-        for idx, row in enumerate(board):
-            temp_1 += row[:3]
-            temp_2 += row[3:6]
-            temp_3 += row[6:]
-            if idx % 3 == 2:
-                if check_row_col([temp_1]) and check_row_col([temp_2]) and check_row_col([temp_3]):
-                    temp_1, temp_2, temp_3 = [], [], []
-                else:
-                    return False
+        # # 3x3 board check
+        # temp_1, temp_2, temp_3 = [], [], []
+        # for idx, row in enumerate(board):
+        #     temp_1 += row[:3]
+        #     temp_2 += row[3:6]
+        #     temp_3 += row[6:]
+        #     if idx % 3 == 2:
+        #         if check_row_col([temp_1]) and check_row_col([temp_2]) and check_row_col([temp_3]):
+        #             temp_1, temp_2, temp_3 = [], [], []
+        #         else:
+        #             return False
         
+        # return True
+        big = set()
+        for i in xrange(0,9):
+            for j in xrange(0,9):
+                if board[i][j]!='.':
+                    cur = board[i][j]
+                    if (i,cur) in big or (cur,j) in big or (i/3,j/3,cur) in big:
+                        return False
+                    big.add((i,cur))
+                    big.add((cur,j))
+                    big.add((i/3,j/3,cur))
         return True
