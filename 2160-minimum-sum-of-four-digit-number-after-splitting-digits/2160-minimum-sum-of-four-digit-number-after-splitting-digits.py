2a198374aaf748abd3bdc309d3acd67cec99b093
Dongryul Min <101853960+MinDongRyul@users.noreply.github.com>
2024-06-08 14:34:32 +0900

    Time: 8 ms (88.57%), Space: 11.4 MB (99.05%) - LeetHub

--- 2160-minimum-sum-of-four-digit-number-after-splitting-digits/2160-minimum-sum-of-four-digit-number-after-splitting-digits.py
+++ 2160-minimum-sum-of-four-digit-number-after-splitting-digits/2160-minimum-sum-of-four-digit-number-after-splitting-digits.py
@@ -4,11 +4,14 @@ class Solution(object):
         :type num: int
         :rtype: int
         """
-        cnt1, cnt2 = '', ''
-        sorted_num = sorted(str(num))
-        for idx, num in enumerate(sorted_num):
-            if idx % 2 == 0:
-                cnt1 += num
-            else:
-                cnt2 += num
-        return int(cnt1) + int(cnt2)
+#         cnt1, cnt2 = '', ''
+#         sorted_num = sorted(str(num))
+#         for idx, num in enumerate(sorted_num):
+#             if idx % 2 == 0:
+#                 cnt1 += num
+#             else:
+#                 cnt2 += num
+#         return int(cnt1) + int(cnt2)
+
+        num = sorted(str(num))
+        return int(num[0]) * 10 + int(num[1]) * 10 + int(num[2]) + int(num[3])
