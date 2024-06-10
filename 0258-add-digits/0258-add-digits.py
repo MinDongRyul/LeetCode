bd7f86179dc5f07ff97f49a81936972536a8fb8b
Dongryul Min <101853960+MinDongRyul@users.noreply.github.com>
2024-06-10 20:21:24 +0900

    Time: 18 ms (46.08%), Space: 11.6 MB (70.02%) - LeetHub

--- 0258-add-digits/0258-add-digits.py
+++ 0258-add-digits/0258-add-digits.py
@@ -1,23 +1,9 @@
-# def totalReturn(num):
-#     temp = [int(val) for val in str(num)]
-#     total = sum(temp)
-#     return total
-
-def totalReturn2(num):
-    temp = [int(val) for val in str(num)]
-    total = sum(temp)
-    if total >= 10:
-        return totalReturn2(total)
-    return total
-
 class Solution(object):
     def addDigits(self, num):
         """
         :type num: int
         :rtype: int
         """
-        total = totalReturn2(num)
-        # while total >= 10:
-        #     total = totalReturn(num)
-        #     num = total
-        return total
+        while len(str(num)) > 1:
+            num = sum(map(int, str(num)))
+        return num
