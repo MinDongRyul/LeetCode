a7c0f8bcb04d4efda8f32746b0c5ae3054223efe
Dongryul Min <101853960+MinDongRyul@users.noreply.github.com>
2024-06-18 21:31:41 +0900

    Time: 18 ms (79.39%), Space: 11.6 MB (49.62%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.py
+++ 1029-two-city-scheduling/1029-two-city-scheduling.py
@@ -4,7 +4,12 @@ class Solution(object):
         :type costs: List[List[int]]
         :rtype: int
         """
-        for idx, cost in enumerate(costs):
-            costs[idx].append(cost[0]-cost[1])
-        costs = sorted(costs, key=lambda x : x[-1])
-        return sum(cost[0] if idx + 1 <= len(costs)//2 else cost[1] for idx, cost in enumerate(costs))
+        # my answer
+        # for idx, cost in enumerate(costs):
+        #     costs[idx].append(cost[0]-cost[1])
+        # costs = sorted(costs, key=lambda x : x[-1])
+        # return sum(cost[0] if idx + 1 <= len(costs)//2 else cost[1] for idx, cost in enumerate(costs))
+ 
+        sm=sum([i for i,j in costs])
+        diff=sorted([j-i for i,j in costs])
+        return sm+sum(diff[:len(costs)//2])
