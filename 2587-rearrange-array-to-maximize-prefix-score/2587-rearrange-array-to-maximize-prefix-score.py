bfce5d6a113c4e7dbc189d41b4d8fc1cbe7ba20c
Dongryul Min <101853960+MinDongRyul@users.noreply.github.com>
2024-06-20 23:00:09 +0900

    Time: 560 ms (82.14%), Space: 23.8 MB (28.57%) - LeetHub

--- 2587-rearrange-array-to-maximize-prefix-score/2587-rearrange-array-to-maximize-prefix-score.py
+++ 2587-rearrange-array-to-maximize-prefix-score/2587-rearrange-array-to-maximize-prefix-score.py
@@ -4,13 +4,25 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        
-        temp, r = 0, 0
+        # fix first code
+        temp = 0
         nums = sorted(nums, reverse=True)
-        
         for idx, num in enumerate(nums):
             nums[idx] = num + temp
             temp = nums[idx]
             if temp <= 0:
                 return idx
         return len(nums)
+
+        # first code
+        # temp, r = 0, 0
+        # nums = sorted(nums, reverse=True)
+        
+        # for idx, num in enumerate(nums):
+        #     nums[idx] = num + temp
+        #     temp = nums[idx]
+            
+        #     if temp > 0:
+        #         r += 1
+            
+        # return r
