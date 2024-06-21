c3c6f0ae60bfcf797bfa6548ba50c5f35cf4c594
Dongryul Min <101853960+MinDongRyul@users.noreply.github.com>
2024-06-21 22:56:38 +0900

    Time: 19 ms (75.17%), Space: 11.8 MB (67.87%) - LeetHub

---
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def runningSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        temp = 0
+        for idx, num in enumerate(nums):
+            nums[idx] = temp + num
+            temp = nums[idx]
+        return nums
