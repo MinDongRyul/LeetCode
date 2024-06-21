54e70b4699f1eb47babcfdb5737b213d3586cc87
Dongryul Min <101853960+MinDongRyul@users.noreply.github.com>
2024-06-21 22:58:31 +0900

    Time: 25 ms (27.85%), Space: 11.8 MB (67.87%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
@@ -4,8 +4,12 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        temp = 0
-        for idx, num in enumerate(nums):
-            nums[idx] = temp + num
-            temp = nums[idx]
+        # temp = 0
+        # for idx, num in enumerate(nums):
+        #     nums[idx] = temp + num
+        #     temp = nums[idx]
+        # return nums
+
+        for idx in range(1, len(nums)):
+            nums[idx] += nums[idx-1]
         return nums
