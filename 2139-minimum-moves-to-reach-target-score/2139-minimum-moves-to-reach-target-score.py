60ef105ea9a26afec559c18417946150a8d4251b
Dongryul Min <101853960+MinDongRyul@users.noreply.github.com>
2024-06-23 10:06:30 +0900

    Time: 3 ms (100%), Space: 11.6 MB (56.41%) - LeetHub

---
+++ 2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def minMoves(self, target, maxDoubles):
+        """
+        :type target: int
+        :type maxDoubles: int
+        :rtype: int
+        """
+        cnt = 0
+        while maxDoubles > 0 and target > 1: 
+            if target % 2 == 0:
+                target //= 2
+                maxDoubles -= 1
+                cnt += 1
+            else:
+                target -= 1
+                cnt += 1
+                
+        return cnt + target-1
