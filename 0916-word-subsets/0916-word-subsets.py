d9ce47d13a94234463debcc4dcfdea7bd1af258f
Dongryul Min <101853960+MinDongRyul@users.noreply.github.com>
2024-08-05 01:07:38 +0900

    Time: 1573 ms (17.54%), Space: 16.4 MB (33.33%) - LeetHub

---
+++ 0916-word-subsets/0916-word-subsets.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def wordSubsets(self, words1, words2):
+        """
+        :type words1: List[str]
+        :type words2: List[str]
+        :rtype: List[str]
+        """
+        
+        # include_dict = defaultdict(int)
+        
+        # for word2 in words2:
+        #     temp = Counter(word2)
+        #     for word in temp:
+        #         include_dict[word] += temp[word] 
+        
+        # rm_idx = []
+        # for word1 in words1:
+        #     temp = Counter(word1)
+        #     for word in include_dict:
+        #         if include_dict[word] > temp[word]:
+        #             rm_idx.append(words1.index(word1))
+                    
+        # rm_idx = sorted(set(rm_idx), reverse=True)
+        # for idx in rm_idx:
+        #     words1.pop(idx)
+            
+        # return words1
+
+        count = collections.Counter()
+        for b in words2:
+            count |= collections.Counter(b)
+        # not count - Counter(a) : count에서 Counter(a)를 빼고 남는게 없어야 count를 전부 포함하는 word라는 의미
+        return [a for a in words1 if not count - Counter(a)]
