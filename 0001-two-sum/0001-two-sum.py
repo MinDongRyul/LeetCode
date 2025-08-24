5c49870028cc7f1cb01147e659ffa6ba4d37fd9e
Dongryul Min <101853960+MinDongRyul@users.noreply.github.com>
2025-08-24 20:42:14 +0900

    Time: 283 ms (35.62%), Space: 18.3 MB (86.62%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,19 +1,5 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        # rst = []
-        # for idx in range(len(nums)):
-        #     a = nums[idx]
-        #     for idx_2 in range(idx+1, len(nums)):
-        #         b = nums[idx_2]
-        #         if a + b == target:
-        #             rst.append(idx)
-        #             rst.append(idx_2)
-        #             return rst
-        for idx, val in enumerate(nums):
-            if target - val in nums[idx+1:]:
-                return [idx, nums.index(target-val, idx+1, len(nums))]
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for idx, x in enumerate(nums):
+            if (target - x) in nums[idx+1:]:
+                return [idx, nums.index(target-x, idx+1)]
