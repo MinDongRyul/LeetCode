c87d78f952400a2cc20168c0ebb5188d20cf21a7
Dongryul Min <101853960+MinDongRyul@users.noreply.github.com>
2025-08-25 23:39:54 +0900

    Time: 4 ms (8.75%), Space: 18 MB (31.99%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,20 +1,19 @@
-class Solution(object):
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        lst = [str_ for str_ in s]
- 
-        stack = [s[0]]
-
-        for s in lst[1:]:
-            if len(stack) != 0 and stack[-1]+s in ['()', '{}', '[]']:
-                stack.pop(-1)
-            else:
-                stack.append(s)
-            
-        if len(stack) == 0:
-            return True
-        else:
+class Solution:
+    def isValid(self, s: str) -> bool:
+        queue = []
+        dict_ = {')': '(', ']':'[', '}':'{'}
+        for s_ in s:
+            if s_ in ['(', '[', '{']:
+                queue.append(s_)
+            elif s_ in [')', ']', '}']:
+                if not queue:
+                    return False
+                if queue[-1] == dict_[s_]:
+                    queue.pop(-1)
+                else:
+                    queue.append(s_)
+                
+        if queue:
             return False
+        else:
+            return True
